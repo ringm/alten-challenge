45397ed4de2b32151201bf811c07410f2fc2893e
Martin <martin.ring@ecloudsolutions.com>
2025-04-12 10:37:50 +0200

    feat: bag & company icons

---
+++ src/icons/bag.tsx
@@ -0,0 +1,12 @@
+export const BagIcon = () => {
+  return (
+    <svg className="bag" width="13" height="17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.47.32H3.766v3.765H0V16.32h12.235V4.085H8.471V.32Zm-.94 4.706V7.38h.94V5.026h2.824V15.38H.941V5.026h2.824V7.38h.94V5.026H7.53Zm0-.941V1.261H4.705v2.824h2.823Z"
+        fill="#000"
+      />
+    </svg>
+  );
+};
