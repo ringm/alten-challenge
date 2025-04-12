fe154771b3309739cf70f5dac133913d0c6c324e
Martin <martin.ring@ecloudsolutions.com>
2025-04-12 10:44:15 +0200

    feat: product type definition

---
+++ src/types/product.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: string;
+  brand: string;
+  name: string;
+  description: string;
+  basePrice: number;
+  rating: number;
+  specs: {
+    screen: string;
+    resolution: string;
+    processor: string;
+    mainCamera: string;
+    selfieCamera: string;
+    battery: string;
+    os: string;
+    screenRefreshRate: string;
+  };
+  colorOptions: {
+    name: string;
+    hexCode: string;
+    imageUrl: string;
+  }[];
+  storageOptions: {
+    capacity: string;
+    price: number;
+  }[];
+  similarProducts: ProductSummary[];
+}
+
+export type ProductSummary = Pick<Product, "id" | "brand" | "name" | "basePrice"> & {
+  imageUrl: string;
+  colorName: string;
+};
