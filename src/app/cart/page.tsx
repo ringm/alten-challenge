eac015dd7194870ad9f40f30c0f825ea2ef70309
Martin <martin.ring@ecloudsolutions.com>
2025-04-12 16:25:34 +0200

    feat(frontend): cart page improvement

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import { CartCounter } from "@/components/cart/counter";
-import { CartItem } from "@/components/cards/cart-item/cart-item";
-import { useCart } from "@/hooks/useCart";
-import { useIsClient } from "@/hooks/useIsClient";
-import Link from "next/link";
-
+import { CartCounter } from "@/components/cart/counter/counter";
+import { CartItemsList } from "@/components/cart/item-list/item-list";
+import { CartSummary } from "@/components/cart/summary/cart-summary";
 import s from "./page.module.css";
 
 export default function CartPage() {
-  const cart = useCart();
-  const isClient = useIsClient();
-  const cartTotal = cart.items.reduce((total, item) => total + item.basePrice, 0);
   return (
     <div className={s.wrapper}>
       <h1 className={s.title}>
@@ -20,27 +12,8 @@ export default function CartPage() {
           (<CartCounter />)
         </span>
       </h1>
-      {isClient && (
-        <>
-          <div className={s.cart_list}>
-            {cart.items?.map((item, idx) => (
-              <CartItem key={`${item.id}-${idx}`} {...item} />
-            ))}
-          </div>
-          <div className={s.cart_footer}>
-            <div className={s.cart_total}>
-              <p>total</p>
-              <p>{cartTotal} EUR</p>
-            </div>
-            <div className={s.cart_actions}>
-              <Link className={s.continue_btn_link} href={{ pathname: "/" }}>
-                <button className={s.continue_btn}>continue shopping</button>
-              </Link>
-              <button className={s.pay_btn}>pay</button>
-            </div>
-          </div>
-        </>
-      )}
+      <CartItemsList />
+      <CartSummary />
     </div>
   );
 }
