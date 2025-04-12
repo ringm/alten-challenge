b1422de268912a2fc36426a02a91eadd2df359af
Martin <martin.ring@ecloudsolutions.com>
2025-04-12 10:37:33 +0200

    feat: nav

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { CompanyLogo } from "@/icons/company-logo";
+import { BagIcon } from "@/icons/bag";
+import { CartCounter } from "../cart/counter";
+import styles from "./nav.module.css";
+
+export const Nav: React.FC = () => {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <CompanyLogo />
+      </Link>
+      <Link href="/cart">
+        <div className={styles.cart}>
+          <BagIcon />
+          <CartCounter />
+        </div>
+      </Link>
+    </nav>
+  );
+};
